19801763401a8440f76a4871fccdc69abfbf0ad4
rstachura <rstachura@RYAN_XPS>
2023-03-08 09:48:49 -0500

    prevent program from hanging while reading

--- request-response.py
+++ request-response.py
@@ -1,15 +1,34 @@
+import signal
 from serial import Serial
 from utils import contsruct_payload_from_json, get_json_from_packet, PACKET_FOOTER                    
 
+serial_port:Serial = None
+
+def exit_handler(sig, frame):
+    global serial_port
+    print(f"Signal caught:")
+    if serial_port and serial_port.is_open:
+        print("    Closing port...")
+        serial_port.close()
+    quit("    Quitting...")
+
+
 def main():
-    port_name = '/dev/ttys016'
+    global serial_port
+    signal.signal(signal.SIGINT, exit_handler)
+    signal.signal(signal.SIGTERM, exit_handler)
+
+    port_name = 'COM11'
+    # port_name = '/dev/ttys016'
     # port_name = '/dev/tty.usbserial-54790373251'
 
-    s = Serial(port_name, baudrate=115200)
+    serial_port = Serial(port_name, baudrate=115200, timeout=0.2)
     msg = contsruct_payload_from_json('{"command":"get_commands"}')
-    s.write(msg)
+    serial_port.write(msg)
 
-    resp_packet = s.read_until(PACKET_FOOTER)
+    resp_packet = None
+    while not resp_packet:
+        resp_packet = serial_port.read_until(PACKET_FOOTER)
     resp_json = get_json_from_packet(resp_packet)
     print("Received:", resp_json)    
 
