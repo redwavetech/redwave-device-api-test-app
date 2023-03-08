db338e17e8cb0842888c81f52353b34100ba93b5
rstachura <rstachura@RYAN_XPS>
2023-03-08 09:48:07 -0500

    commented out repetitve print statements

--- utils.py
+++ utils.py
@@ -20,15 +20,15 @@ def get_json_from_packet(packet:bytes):
     # Check packet header
     if packet[:2] != PACKET_HEADER:                     
         raise Warning("INVALID PACKET HEADER")
-    print(f'packet header: {packet[:2]}')        
+    # print(f'packet header: {packet[:2]}')        
 
     json_supposed_len, = unpack('<I', packet[2:6]) 
-    print(f'packet length: {packet[2:6]}')
+    # print(f'packet length: {packet[2:6]}')
         
     # Extract JSON message
     json_bytes = packet[6:-3]
     json_str = json_bytes.decode()                           
-    print(f'packet json: {json_str}')
+    # print(f'packet json: {json_str}')
     
     # Extract alleged length of JSON message 
     # (interpret bytes as an unsigned integer)
@@ -38,7 +38,7 @@ def get_json_from_packet(packet:bytes):
     # Extract CRC
     crc = packet[-3] 
     crc_packet = packet[-3:-2]                                  
-    print(f'packet crc: {crc_packet}')
+    # print(f'packet crc: {crc_packet}')
     if (crc != crc8(json_bytes)):
         raise Warning("CRC MISMATCH")
     
@@ -46,7 +46,7 @@ def get_json_from_packet(packet:bytes):
     packet_footer = packet[-2:]
     if packet_footer != PACKET_FOOTER:                    
         raise Warning("INVALID PACKET FOOTER")    
-    print(f'packet footer: {packet_footer}')
+    # print(f'packet footer: {packet_footer}')
             
     return json_str
 
@@ -59,8 +59,8 @@ def contsruct_payload_from_json(json_str:str):
     print(f'packet header: {PACKET_HEADER}')    
     print(f'packet length: {pack("<I", len(json_str))}')
     print(f'packet json: {json_bytes}')
-    print(f'packet crc: {crc.to_bytes(1, "little", signed=False)}, crc={crc}')
-    print(f'packet crc.to_bytes, {crc.to_bytes(1, "little")}')
+    print(f'packet crc: bytes={crc.to_bytes(1, "little", signed=False)}, int={crc}')
+    # print(f'packet crc.to_bytes, {crc.to_bytes(1, "little")}')
     print(f'packet footer: {PACKET_FOOTER}')    
 
     return b''.join([
