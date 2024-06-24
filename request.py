16dbb674464092c0c54340198e5a54a1c573073e
rstachura <rstachura@RYAN_XPS>
2024-06-24 11:00:32 -0400

    Remove function redefinition

--- request.py
+++ request.py
@@ -1,29 +1,6 @@
-from serial.tools.list_ports import comports
 from serial import Serial
-from struct import pack, unpack
 import argparse
-from utils import crc8, PACKET_HEADER, PACKET_FOOTER
-                        
-def contsruct_payload_from_json(json_str:str):
-
-    json_bytes = json_str.encode()                      # Convert string to bytes
-    crc = crc8(json_bytes)
-
-    print('- - - - Request from app - - - - -') 
-    print(f'packet header: {PACKET_HEADER}')    
-    print(f'packet length: {pack("<I", len(json_str))}')
-    print(f'packet json: {json_bytes}')
-    print(f'packet crc: {crc.to_bytes(1, "little", signed=False)}, crc={crc}')
-    print(f'packet crc.to_bytes, {crc.to_bytes(1, "little")}')
-    print(f'packet footer: {PACKET_FOOTER}')    
-
-    return b''.join([
-        PACKET_HEADER,
-        pack("<I", len(json_str)),                      # Convert length to unsigned integer byte representation        
-        json_bytes,
-        crc.to_bytes(1, 'little', signed=False),        # Convert CRC to byte representation
-        PACKET_FOOTER
-    ])
+from utils import contsruct_payload_from_json
 
 def main():
     port_name = '/dev/cu.usbmodem2101'
